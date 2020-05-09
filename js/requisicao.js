bcbca16fa3d5a73d910d3b5d178cec60b77be233
unknown <carpe@carpetecnologia.com.br>
2020-05-09 14:50:49 -0300

    ajustes

--- js/requisicao.js
+++ js/requisicao.js
@@ -1,4 +1,20 @@
-const url = 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72'
+const url = 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72';
+
+var diferenca =1;
+
+function calcular() {
+    
+    let entrada = document.getElementById('checkin').value;
+    let saida = document.getElementById('checkout').value;
+
+    entrada = !entrada ? new Date() : new Date(document.getElementById('checkin').value);
+    saida = !saida ? new Date() : new Date(document.getElementById('checkout').value);
+
+    var diferenca = Math.abs(saida.getDate() - entrada.getDate());       
+}
+
+
+
 
 fetch(url)
     .then(response => response.json())
@@ -6,19 +22,16 @@ fetch(url)
 
         let elementos = document.getElementById('resultados-quartos');
 
-        for(let i =0; i <24;i++){
+        for (let i = 0; i < 24; i++) {
             elementos.innerHTML += `
             <div class='card-hospedagem-tumb'>
             <div class='card-hospedagem-img'><img src='${data[i].photo}' class='card-hospedagem-img'/></div>
             <div class="card-hospedagem-titulo">${data[i].property_type}</div>
             <div class="card-hospedagem-descricao">${data[i].name}</div>
-                <div class="card-hospedagem-descricao"><b>R$ ${data[i].price}</b>/noite</div>
+            <div class="card-hospedagem-descricao"><b>R$ ${data[i].price}</b>/noite</div>
             </div>
             `;
-      
-         
-                   
-        }  
+        }
     })
 
 
